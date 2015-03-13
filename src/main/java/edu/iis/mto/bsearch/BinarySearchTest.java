f64b480134fe271c77bba2ba1707f6c9235a103c
Lukasz Tracz <kapicel20@wp.pl>
2015-03-13 09:40:01 +0100

    Stworzenie testu pierwszego dla dlugosci sekwencji wejsciowej rownej 1 z zalozeniem ze elementu wyszukiwanego nie ma w sekwencji

--- src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/main/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -9,13 +9,13 @@ public class BinarySearchTest {
 	
 	@BeforeClass
 	public static void setUpBefforeClass() throws Exception{
-	BinarySearchTest serchTest = new BinarySearchTest();
+	
 	}
 	
 	
 	
 	@Test
-	public void IsInSekw() {
+	public void IsInSekq() {
 		int[] seq = new int[1];
 		int key =56;
 		seq[0] =key;
@@ -23,8 +23,22 @@ public class BinarySearchTest {
 		assertTrue(searchResult.isFound());
 		
 		
-		
-
 	}
+	
+	@Test
+	public void IsNotInSekq() {
+		int[] seq = new int[1];
+		int key =56;
+		//seq[0] =key;
+		SearchResult searchResult = BinarySearch.search(key, seq);
+		assertFalse(searchResult.isFound());
+		
+		
+	}
+	
+	
 
+
+	
+	
 }
